78d7006f9dd73152985812d52ab32f312bc7ce18
Jorge Esteban Diaz Bernal <157375038+JorEstDiBe@users.noreply.github.com>
2025-10-15 18:43:47 -0500

    Login

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ front/src/pages/login/Login.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+    return (
+        <div className='contenedor-login'>
+            <div className='contenedor-blanco'>
+                {/* Panel derecho con imagen */}
+                    <div className="contenedor-imagen">
+                        <img
+                            src="/assets/user.png"
+                            alt="User"
+                            className="imagen_user"
+                        />
+                    </div>
+
+                {/* Panel derecho con formulario */}
+                    <div className="contenedor-texto">
+                        <h1 className="titulo">Log In</h1>
+                        <form className="formulario-login">
+                            <label htmlFor="usuario">Usuario</label>
+                            <input
+                            type="text"     
+                            id="usuario"
+                            placeholder="Usuario"
+                            />
+
+                            <label htmlFor="password">Contraseña</label>
+                            <input
+                            type="password"
+                            id="password"
+                            placeholder="********"
+                            />
+
+                            <button type="submit" className="btn-login"> Ingresar </button>
+                        </form>
+                    </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
