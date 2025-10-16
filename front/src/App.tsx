fb990910685c44a5eea99ccc064201c6465ff1a6
Jorge Esteban Diaz Bernal <157375038+JorEstDiBe@users.noreply.github.com>
2025-10-15 22:42:08 -0500

    Comentarios front completo

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,6 +5,7 @@ import Signin from "./pages/signin/Signin";
 import Solicitud from "./pages/solicitud/Solicitud";
 import Error from "./pages/error/Error";
 import Compra from "./pages/compra/Compra";
+import Comentario from "./pages/comentario/Comentario";
 import "./App.css";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/signin" element={<Signin />} />
         <Route path="/solicitud" element={<Solicitud />} />
         <Route path="/compra" element={<Compra />} />
+        <Route path="/comentario" element={<Comentario />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
