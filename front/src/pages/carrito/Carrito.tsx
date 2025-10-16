1eca4385909f298963844b5ce782456a7a067d3a
juancego <juancego@unisabana.edu.co>
2025-10-16 18:35:13 -0500

    Interfaz carrito

--- front/src/pages/carrito/Carrito.tsx
+++ front/src/pages/carrito/Carrito.tsx
@@ -1,52 +1,63 @@
 import React from 'react';
 import './Carrito.css';
+import logo_blanco from '../../assets/logo_blanco.jpg';
+import carrito from '../../assets/carrito.jpg';
+import perfil from '../../assets/perfil.png';
+import arepas from '../../assets/arepas_plato.png';
+
 
 const Carrito = () => {
   return (
     <div className="contenedor-carrito">
-      <h2 className="titulo-carrito">Tu carrito de compras</h2>
-
-      <div className="tabla-carrito">
-        <div className="encabezado">
-          <span>Producto</span>
-          <span>Precio</span>
-          <span>Cantidad</span>
-          <span>Total</span>
+      <header className="navbar">
+        <div className="logo-container">
+          <img src={logo_blanco} alt="Logo Arepabuelas" className="logo" />
+          <h1 className="nombre">Arepabuelas</h1>
         </div>
 
-        <hr className="divider" />
+        <div className="iconos">
+          <img src= {carrito} alt="Carrito" className="icono" />
+          <img src={perfil} alt="Perfil" className="icono" />
+        </div>
+      </header>
 
-        <div className="producto">
-          <div className="producto-info">
-            <img
-              src="front\src\assets\arepas_plato.png"
-              alt="Arepa Boyacense"
-              className="producto-img"
-            />
-            <div className="producto-detalle">
-              <p className="producto-nombre">Arepa Boyacense</p>
-              <a href="#" className="quitar">Quitar</a>
+      <main className="contenido-carrito">
+        <h2>Tu carrito de compras</h2>
+
+        <div className="tabla-carrito">
+          <div className="encabezado-tabla">
+            <span>Producto</span>
+            <span>Precio</span>
+            <span>Cantidad</span>
+            <span>Total</span>
+          </div>
+
+          <div className="item-carrito">
+            <div className="producto">
+              <img src={arepas} alt="Arepa Boyacense" className="img-producto" />
+              <div className="detalle-producto">
+                <p className="nombre-producto-carrito">Arepa Boyacense</p>
+                <a href="#" className="quitar">Quitar</a>
+              </div>
             </div>
+
+            <span className="precio">COP 5,000</span>
+
+            <div className="cantidad">
+              <button>-</button>
+              <span>1</span>
+              <button>+</button>
+            </div>
+
+            <span className="total">COP 5,000</span>
           </div>
-
-          <span className="precio">COP 5,000</span>
-
-          <div className="cantidad">
-            <button className="btn-cantidad">+</button>
-            <span>1</span>
-            <button className="btn-cantidad">-</button>
-          </div>
-
-          <span className="total">COP 5,000</span>
         </div>
 
-        <hr className="divider" />
-
-        <div className="botones">
+        <div className="botones-carrito">
           <button className="btn-regresar">Regresar</button>
           <button className="btn-comprar">Comprar</button>
         </div>
-      </div>
+      </main>
     </div>
   );
 };
