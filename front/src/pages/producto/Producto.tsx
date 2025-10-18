d823b4595b3429aa8cd7eac501f0c85f51aa3e79
MariSalas23 <mari.masagu@gmail.com>
2025-10-17 19:43:11 -0500

    Producto front listo

--- front/src/pages/producto/Producto.tsx
+++ front/src/pages/producto/Producto.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import './Producto.css';
 import imgProducto from '../../assets/productoejemplo.png';
+import logo_blanco from '../../assets/logo_blanco.jpg';
+import carrito from '../../assets/carrito.jpg';
+import perfil from '../../assets/perfil.png';
 
 const Producto = () => {
   return (
-    <div className="contenedor-producto">
+
+    <div className="contenedor-producto-header">
+        <header className="navbar-carrito">
+            <div className="logo-container-carrito">
+                <img src={logo_blanco} alt="Logo Arepabuelas" className="logo-carrito" />
+                <h1 className="nombre-carrito">Arepabuelas</h1>
+            </div>
+            <div className="iconos-carrito">
+                <img src= {carrito} alt="Carrito" className="icono-carrito" />
+                <img src={perfil} alt="Perfil" className="icono-carrito " />
+            </div>
+        </header>
+      <div className="contenedor-producto">
         <div className="productos-producto">
             <div className="contenedor-titulo-producto">
-                <h1 className="nombre-producto">Nombre</h1>
-                <h1 className="precio-producto">Precio COP</h1>
+                <h1 className="nombre-producto">Arepa Boyacense</h1>
+                <h1 className="precio-producto">5,000 COP</h1>
             </div>
             <div className="contenedor-imagen-producto">
                 <img src={imgProducto} alt="Producto" />
             </div>
-            <p className="descripcion-producto">Descripción del producto</p>
+            <p className="descripcion-producto">Disfruta el sabor auténtico de Boyacá con nuestras arepas
+                                                boyacenses, elaboradas artesanalmente con maíz amarillo molido,
+                                                cuajada fresca y un toque de mantequilla campesina. 
+                                                Su textura suave por dentro y ligeramente dorada por fuera las convierte
+                                                en el acompañamiento perfecto para el desayuno o la merienda.</p>
         </div>
         <div className="comentarios-producto"> 
             <h1 className="titulo-comentario-producto">Comentarios</h1>
@@ -21,7 +40,6 @@ const Producto = () => {
                 <div className="comentario-individual-producto">
                     <h2 className="usuario-comentario-producto">Usuario</h2>
                     <p className="comentario-producto">Comentario acerca del producto</p>
-                    <div className="linea-verde"></div>
                 </div>
             </div>
             <div className="botones-producto">
@@ -29,6 +47,7 @@ const Producto = () => {
                 <button className="btn-comentar-producto">Comentar</button>
                 <button className="btn-carrito-producto">Añadir al carrito</button>
             </div>
+        </div>
         </div>
     </div>
   );
