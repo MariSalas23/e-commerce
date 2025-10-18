f8caea1ae5c28b1610c479d98a2ef760ac3a9b4c
MariSalas23 <mari.masagu@gmail.com>
2025-10-18 14:27:32 -0500

    Admin front listo

--- front/src/pages/admin/Admin.tsx
+++ front/src/pages/admin/Admin.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Admin.css';
 import imgCesta from '../../assets/cesta.png';
 import imgPerfil from '../../assets/perfil.png';
+import imgImagen from '../../assets/imagen.png';
 import logo_blanco from '../../assets/logo_blanco.jpg';
 import carrito from '../../assets/carrito.jpg';
 import perfil from '../../assets/perfil.png';
@@ -11,22 +12,24 @@ const Admin = () => {
     <div className="contenedor-admin">
       <header className="navbar-carrito">
         <div className="logo-container-carrito">
-                  <img src={logo_blanco} alt="Logo Arepabuelas" className="logo-carrito" />
-                  <h1 className="nombre-carrito">Arepabuelas</h1>
-              </div>
-              <div className="iconos-carrito">
-                  <img src= {carrito} alt="Carrito" className="icono-carrito" />
-                  <img src={perfil} alt="Perfil" className="icono-carrito " />
-              </div>
-        </header>
-        <img src= {imgCesta} alt="Arepas" className="arepas-intro" />
+          <img src={logo_blanco} alt="Logo Arepabuelas" className="logo-carrito" />
+          <h1 className="nombre-carrito">Arepabuelas</h1>
+        </div>
+        <div className="iconos-carrito">
+          <img src={carrito} alt="Carrito" className="icono-carrito" />
+          <img src={perfil} alt="Perfil" className="icono-carrito " />
+        </div>
+      </header>
+
+      <img src={imgCesta} alt="Arepas" className="arepas-intro" />
+
       <div className="contenedor-columnas-admin">
         <div className="control-usuarios">
           <h1 className="titulo-control-usuarios">Control de usuarios</h1>
           <h2 className="subtitulo-control-usuarios">Validación de usuarios nuevos</h2>
           <div className="usuario-nuevo">
             <div className="columnas-usuario-nuevo">
-              <img src= {imgPerfil} alt="Perfil" className="imagen-perfil-admin" />
+              <img src={imgPerfil} alt="Perfil" className="imagen-perfil-admin" />
               <div className="texto-usuario-nuevo">
                 <p className="nombre-usuario-nuevo">Nombre</p>
                 <p className="correo-usuario-nuevo">Correo</p>
@@ -38,15 +41,32 @@ const Admin = () => {
             </div>
           </div>
         </div>
+
         <div className="control-productos">
           <h1 className="titulo-control-productos">Control de productos</h1>
           <h2 className="subtitulo-control-productos">Validación de usuarios nuevos</h2>
+
           <div className="producto-nuevo">
             <div className="columnas-producto-nuevo">
+              <img src={imgImagen} alt="Agregar imagen" className="imagen-producto-admin" />
               <div className="nombre-descripcion">
-                </div>
+                <input
+                  type="text"
+                  placeholder="Nombre del producto"
+                  className="nombre-producto-nuevo"
+                />
+                <textarea
+                  placeholder="Descripción del producto"
+                  className="descripcion-producto-nuevo"
+                />
+                <input
+                  placeholder="Precio (COP)"
+                  className="precio-producto-admin"
+                />
+              </div>
             </div>
           </div>
+
           <button className="btn-crear">Crear</button>
         </div>
       </div>
